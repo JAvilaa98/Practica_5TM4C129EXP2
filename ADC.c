63968fc3a4997e2a08d2976ce560a4c42b9e4597
JAvilaa98 <113447760+JAvilaa98@users.noreply.github.com>
2023-01-29 23:53:47 -0600

    Actualización
    Se configuró el ADC para posteriormente realizar las pruebas físicas.

--- ADC.c
+++ ADC.c
@@ -1,66 +1,67 @@
-
 #include "lib/include.h"
+
+/* Habilitamos los 3 canales del módulo 0 del PWM0 para así poder controlar 3 potenciometros
+   a una velocidad de conversión de 250kps según la pág 1056.
+   Canal 1 -> PE2 - secuenciador 0 
+   Canal 2 -> PE1 - secuenciador 1
+   Canal 8 -> PE5 - secuenciador 2 
+    */
 
 extern void Configura_Reg_ADC0(void)
 {
-    /*
-    Habilitar el modulo 0 del ADC con dos canales analogicos 
-    en el puerto E a una velocidad de conversion de 250ksps
-    dandole la mayor prioridad al secuenciador 2 con evento
-    de procesador 
-    */
-     //Pag 396 para inicializar el modulo de reloj del adc RCGCADC
-    SYSCTL->RCGCADC = (1<<0); 
+    SYSCTL->RCGCADC = (1<<0);//Habilitamos el módulo 0 de reloj del ADC pág 369
     //Pag 382 (RGCGPIO) Puertos base habilitación del reloj
     //                     F     E      D       C      B     A
-    SYSCTL->RCGCGPIO |= (1<<5)|(1<<4)|(0<<3)|(0<<2)|(0<<1)|(1<<0)|(1<<12)|(1<<8);
-    //Pag 760 (GPIODIR) Habilta los pines como I/O un cero para entrada y un uno para salida
-    GPIOE_AHB->DIR = (0<<5) | (0<<4); //PE5 y PE4
-    //(GPIOAFSEL) pag.770 Enable alternate función para que el modulo analógico tenga control de esos pines
-    GPIOE_AHB->AFSEL =  (1<<4) | (1<<5 );
-    //(GPIODEN) pag.781 desabilita el modo digital
-    GPIOE_AHB->DEN = (0<<4) | (0<<5 );
-    //Pag 787 GPIOPCTL registro combinado con el GPIOAFSEL y la tabla pag 1808
-    GPIOE_AHB->PCTL = GPIOE_AHB->PCTL & (0xFF00FFFF);
-    //(GPIOAMSEL) pag.786 habilitar analogico
-    GPIOE_AHB->AMSEL = (1<<5) | (1<<4);
-    //Pag 1159 El registro (ADCPC) establece la velocidad de conversión por segundo
-    ADC0->PC = (0<<2)|(0<<1)|(1<<0);//250ksps
-    //Pag 1099 Este registro (ADCSSPRI) configura la prioridad de los secuenciadores
-    ADC0->SSPRI = 0x3210;
-    //Pag 1077 (ADCACTSS) Este registro controla la activación de los secuenciadores
-    ADC0->ACTSS  =   (0<<3) | (0<<2) | (0<<1) | (0<<0);
-    //Pag 1091 Este registro (ADCEMUX) selecciona el evento que activa la conversión (trigger)
-    ADC0->EMUX  = (0x0000);
-    //Pag 1129 Este registro (ADCSSMUX2) define las entradas analógicas con el canal y secuenciador seleccionado
-    ADC0->SSMUX2 = 0x0089;
-    //pag 868 Este registro (ADCSSCTL2), configura el bit de control de muestreo y la interrupción
-    ADC0->SSCTL2 = (1<<6) | (1<<5) ;
-    /* Enable ADC Interrupt */
-    ADC0->IM |= (1<<2); /* Unmask ADC0 sequence 2 interrupt pag 1082*/
-    //NVIC_PRI4_R = (NVIC_PRI4_R & 0xFFFFFF00) | 0x00000020;
-    //NVIC_EN0_R = 0x00010000;
-    //Pag 1077 (ADCACTSS) Este registro controla la activación de los secuenciadores
-    ADC0->ACTSS = (0<<3) | (1<<2) | (0<<1) | (0<<0);
-    ADC0->PSSI |= (1<<2);
-}
-extern void ADC0_InSeq2(uint16_t *Result){
-        //uint16_t Rojo;
-    //ADC Processor Sample Sequence Initiate (ADCPSSI)
-       ADC0->PSSI = 0x00000004;
-       while((ADC0->RIS&0x04)==0){}; // espera al convertidor
-       Result[1] = ADC0->SSFIFO2&0xFFF; //  Leer  el resultado almacenado en la pila2
-       Result[0] = ADC0->SSFIFO2&0xFFF;
-       //Rojo = (uint16_t)readString('%');
-       //printChar('A');
-        PWM1->_3_CMPB = (int)((1*Result[0])/4095) - 1;
-        PWM1->_3_CMPA = (int)((1*Result[1])/4095) - 1;
-        PWM1->_2_CMPA = 0;
-       ADC0->ISC = 0x0004;  //Conversion finalizada
+    SYSCTL->RCGCGPIO |= (1<<5)|(1<<4)|(0<<3)|(0<<2)|(0<<1)|(1<<0);//Encendemos los puertos necesarios pág 382
+    GPIOE_AHB->DIR = (0<<1) | (0<<2) | (0<<5); //Configura i/o PE1, PE2 y PE5 pág 760
+    GPIOE_AHB->AFSEL =  (1<<1) | (1<<2) | (1<<5); //Habilitamos la función alternativa PE1, PE2 y PE5 pág 770
+    GPIOE_AHB->DEN = (0<<1) | (0<<2) | (0<<5); //Para indicar si es digital o no PE1, PE2 y PE5 pág 781
+    GPIOE_AHB->PCTL = GPIOE_AHB->PCTL & (0xFF0FF00F); //Poner 0 en los bits de los pines 1, 2, 5 pág 787 y tabla pág 1808
+    GPIOE_AHB->AMSEL = (1<<1) | (1<<2) | (1<<5); //Habilitamos el canal analógico PE1, PE2 y PE5 pág 786
 
+    ADC0->PC = (0<<2)|(0<<1)|(1<<0);//Establecemos la velocidad de conversión por segundo - 250ksps: Se pone un 1 en binario para indicar que los 2 millos de muestrs por s se divide /8 para obtener 250ksps pág 1159
+    ADC0->SSPRI = 0x3210;//Prioridad de los secuenciadores (mayor prioridad en el secuenciador 0) pág 1099
+    ADC0->ACTSS  =   (0<<3) | (0<<2) | (0<<1) | (0<<0);//Desactivamos los secuenciadores pág 1077
+    ADC0->EMUX  = (0x0000);//Seleccionamos el evento que activa la conversión pág 1091, se configura por procesador 
+
+    //Este registro (ADCSSMUX2) define las entradas analógicas con el canal y secuenciador seleccionado pág 1129
+    ADC0->SSMUX0 = (1<<0); //Secuenciador 0 se asignar el canal 1
+    ADC0->SSMUX1 = (2<<0); //Secuenciador 1 se asignar el canal 2
+    ADC0->SSMUX2 = (8<<0); //Secuenciador 2 se asignar el canal 8
+
+    //Este registro (ADCSSCTL2), configura el bit de control de muestreo y la interrupción pág 1130
+    ADC0->SSCTL0 = (1<<2) | (1<<1);
+    ADC0->SSCTL1 = (1<<2) | (1<<1);
+    ADC0->SSCTL2 = (1<<2) | (1<<1);
+    
+    //Enable ADC Interrupt
+    ADC0->IM |= (1<<0) | (1<<1) | (1<<2); /* Unmask ADC0 sequence 0, 1 y 2 interrupt pag 1082*/
+    //Este registro controla la activación de los secuenciadores pág 1077 
+    ADC0->ACTSS = (0<<3) | (1<<2) | (1<<1) | (1<<0); //Habilitamos secuenciadores 1 y 2 
+    ADC0->PSSI |= (1<<0) | (1<<1) | (1<<2);  //Conf. por procesador
 }
 
+extern void ADC0_InSeq2(uint16_t *Result,uint16_t *duty)
+{
+    //Habilitamos el modo de configuración
+    ADC0->PSSI |= (1<<0) | (1<<1) | (1<<2);  //Por procesador
+      
+    while((ADC0->RIS&0x04)==0){};  //Espera al convertidor
+       
+    //Secuenciador 0 - Canal 1 - PE2
+    Result[0] = ADC0->SSFIFO0&0xFFF; // En el FIFO0 se almacenan las muestras del secuenciador 0, por lo que se lee, pag 860 
+    duty[0] = (Result[0]*50000)/4096; //Cuentas para 20ms 
+    
+    //Secuenciador 1 - Canal 2 - PE1
+    Result[1] = ADC0->SSFIFO1&0xFFF; // En el FIFO0 se almacenan las muestras del secuenciador 1
+    duty[1] = (Result[1]*50000)/4096; //Cuentas para 20ms
+     
+    //Secuenciador 2 - Canal 8 - PE5
+    Result[2] = ADC0->SSFIFO2&0xFFF;// En el FIFO0 se almacenan las muestras del secuenciador 2
+    duty[2] = (Result[2]*50000)/4096;//Cuentas para 20ms
+    
+    ADC0->ISC = (1<<0) | (1<<1) | (1<<2);  //Conversion finalizada
 
-
+}
 
 
